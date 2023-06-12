664444cccc16a0749949c056fde812925cf73e28
kdjghiurg <135955176+kdjghiurg@users.noreply.github.com>
2023-06-12 23:53:53 +0800

    Update new.py

--- new.py
+++ new.py
@@ -2,18 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 
 def read( word ):
-    url = f'https://crptransfer.moe.gov.tw/index.jsp?SN={word}#資料類型'
+    url = f'https://crptransfer.moe.gov.tw/index.jsp?SN={word}#searchL'
 
     html = requests.get( url )
     bs = BeautifulSoup(html.text,'lxml')
-    data = bs.find('th', axis="資料類型")
+    data = bs.find_all('td')
     try:
-        row = data.find_all('tr')[2]
-        chinese = row.find('cr').text
-        phones = row.find_all('code')
+        row = data.[1]
+        chinese = row.text
+        two = data[4]
+        phones = two
         phone = [e.text for e in phones]
         s = " ".join( phone )
         # s = row.find('sub')
-        return( chinese , s)
+        return( chinese + s)
     except:
         return( '查無此字' )
